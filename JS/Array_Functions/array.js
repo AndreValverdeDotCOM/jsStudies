80009eeba3587790ac024212ffbba8326025aae7
André Valverde <136364624+VerdesuCodes@users.noreply.github.com>
2023-12-14 13:57:14 -0300

    Add files via upload

---
+++ JS/Array_Functions/array.js
@@ -0,0 +1,25 @@
+let vaga = [0, 4, 7, 8]
+
+// armazena o valor '5' no índice '4'
+vaga[4] = 5
+
+//Cria um espaço a mais no array e armazena o valor '7'
+vaga.push(7)
+
+//Quantidade de elementos no array
+vaga.length
+
+//Ordena em ordem crescente
+vaga.sort
+
+//Índice do valor '4'
+vaga.indexOf(4)
+
+for (let i = 0; i <= vaga.length; i++) {
+    console.log(vaga[i])
+}
+
+
+for (let pos in vaga) {
+    console.log(vaga[pos])
+}
